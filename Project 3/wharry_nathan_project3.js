d6a3f83b96b7237983b03cd3f97c9e212ec27b2d
Nathan Wharry <nobrakes@fullsail.edu>
2013-06-19 11:40:26 -0500

    Initial Code Write
    Beginning declaration of variables and beginning of story.

--- Project 3/wharry_nathan_project3.js
+++ Project 3/wharry_nathan_project3.js
@@ -1,1 +1,28 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+
+// define gloabl variables
+
+var oldCop = "Frank";
+	newCopy = "Max";
+	jsonData = json(); // replace with actual json code
+	objLicenseInfo = {
+		"license" : [
+			"name" 		: "Jim Stewart",
+			"address" 	: "123 Oak Street",
+			"plate"		: 123456,
+			"vehicle"	: {
+				"make" 	: "Chevy",
+				"model" : "Colorado"
+			}
+		];
+			
+	};
+	
+
+
+
+
+
+// begin story elements
+
+console.log("As " + oldCop + " and " + newCop + " took off, " + newCop + " pulled out the list of possible suspects and went through them. He quickly rattled off the names, addresses, and the make and models of the possible vehicles.");
